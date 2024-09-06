9221a835a8727dd74278896b4b5c278ff1039d34
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-09-06 16:09:31 -0700

    Add go docs

--- math.go
+++ math.go
@@ -1,5 +1,9 @@
+// Package math is a part of my exercises with Go package and module management
+// It's part of chapter's 10 1-3 exercises in "Learning Go" book
 package math
 
+// Add adds to int values and returns result
+// [addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(val1, val2 int) int {
 	return val1 + val2
 }
