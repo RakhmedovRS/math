5c83720a00f23e70a6a27dc0b4dfd4d3678f6716
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-09-06 17:00:37 -0700

    Add Number interface and update Add method

--- math.go
+++ math.go
@@ -2,8 +2,14 @@
 // It's part of chapter's 10 1-3 exercises in "Learning Go" book
 package math
 
-// Add adds to int values and returns result
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add adds to Number values and returns result
 // [addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(val1, val2 int) int {
+func Add[T Number](val1, val2 T) T {
 	return val1 + val2
 }
